7dd347241e5e79f18c071b3b624a9d6483e4ab90
Jatesh Parikh <jateshparikh97@gmail.com>
2024-05-21 22:14:56 +0530

    hero section done

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,5 +1,33 @@
-const GetStarted = () => {
-  return <div></div>;
-};
+import styles from "../style";
+import { arrowUp } from "../assets";
+
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    >
+      <div className={`${styles.flexStart} flex-row gap-1`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-600 to-gray-50">
+            Get
+          </span>
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrow-up"
+          className="w-[23px] h-[23px] object-contain"
+        />
+      </div>
+
+      <p className="font-poppins font-medium text-[18px] leading-[23px] ml-1">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-600 to-gray-50">
+          Started
+        </span>
+      </p>
+    </div>
+  </div>
+);
 
 export default GetStarted;
